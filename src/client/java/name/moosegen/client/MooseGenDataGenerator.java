25749208b5460d0112c5df8f8691fe5df06a31a9
TheCosmicMoose <hiimashebravo@gmail.com>
2026-06-15 10:23:13 -0400

    initial commit

---
+++ src/client/java/name/moosegen/client/MooseGenDataGenerator.java
@@ -0,0 +1,11 @@
+package name.moosegen.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MooseGenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
